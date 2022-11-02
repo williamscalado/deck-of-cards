fc1ae834bd14e1f85e5022bf47c608108b814ccf
Williams Calado <williamscalado@gmail.com>
2022-11-02 12:03:49 -0300

    improvimentes UI mobile header

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -15,7 +15,6 @@ export const ContainerHeader = styled("div")`
 	@media (max-width: 700px) {
 		width: 95vw;
 		display: grid;
-		justify-content: grid;
 
 		img {
 			margin: auto;
@@ -32,43 +31,14 @@ export const ContainerUser = styled("div")`
 	color: var(--white);
 	border-radius: 1rem;
 	padding: 0.2rem;
-
 	font-weight: 400;
-
-	button {
-		background: var(--red-600);
-		padding: 0.2rem 1rem;
-		margin-left: 1rem;
-		border: 0rem;
-		font-weight: 700;
-		border-radius: 99rem;
-		font-size: 1rem;
-		color: var(--bs-white);
-		transition: filter 0.3s;
-		box-shadow: 0.125rem 0.1875rem 0.0625rem var(--red-800);
-		text-shadow: 0.0625rem 0.0625rem 0.125rem var(--red-800);
-
-		&:hover {
-			filter: brightness(0.9);
-		}
-
-		&:disabled {
-			opacity: 0.4;
-		}
-	}
 
 	@media (max-width: 700px) {
 		display: flex;
 		width: 90vw;
 		height: 3.125rem;
-		justify-content: space-between;
 		border-radius: 0.5rem;
-		font-size: 0.7rem;
-
-		img {
-			width: 150px;
-			padding: 1rem;
-		}
+		font-size: 1rem;
 	}
 `;
 
@@ -94,7 +64,7 @@ export const ContainerMenu = styled("div")`
 		font-weight: 400;
 		height: 35px;
 		border-radius: 1rem;
-		font-size: 1rem;
+		font-size: 0.9rem;
 		color: var(--bs-white);
 		transition: background 0.3s;
 
